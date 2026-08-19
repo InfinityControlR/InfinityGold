64f49c04fd4a8e56f297d735e9376e401dc252b0
InfinityControlR <273343412+InfinityControlR@users.noreply.github.com>
2026-08-20 00:09:38 +0200

    Pin automatic Alchemy fix

--- loader.lua
+++ loader.lua
@@ -81,8 +81,8 @@ end
 -- Only this loader itself is served from main.
 local UI = 'https://raw.githubusercontent.com/InfinityControlR/InfinityGold/11d313ac902b4c025608242b8b5e334c120b7c77/ui/InfinityUI.lua'
 local COMMON = 'https://raw.githubusercontent.com/InfinityControlR/InfinityGold/c80662d1080f7dcf72d4bf89bec42861b7733d3b/games/magicloot_common.lua'
-local LOCOMOTION = 'https://raw.githubusercontent.com/InfinityControlR/InfinityGold/f04eaeef992cb5bc6f6c8bae11c213c12e9bddbb/games/magicloot_locomotion.lua'
-local CORE = 'https://raw.githubusercontent.com/InfinityControlR/InfinityGold/f04eaeef992cb5bc6f6c8bae11c213c12e9bddbb/games/magicloot.lua'
+local LOCOMOTION = 'https://raw.githubusercontent.com/InfinityControlR/InfinityGold/e3b859c35b7a07eeefa32c33c87ce36dafe99980/games/magicloot_locomotion.lua'
+local CORE = 'https://raw.githubusercontent.com/InfinityControlR/InfinityGold/e3b859c35b7a07eeefa32c33c87ce36dafe99980/games/magicloot.lua'
 
 -- Belt and braces: a fresh cache key per run for the pinned URLs too.
 local CACHE_BUST = '?t=' .. tostring(os.time())
